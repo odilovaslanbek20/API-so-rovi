ef4a0c625f0029db81d2cfb807c30dae6809d5a9
odilovaslanbek20 <aslanbekodilov524@gmail.com>
2025-03-23 13:50:46 +0500

    "aaaa"

--- src/companents/Details.jsx
+++ src/companents/Details.jsx
@@ -1,31 +1,36 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import { useParams } from "react-router-dom";
 
 const Details = () => {
   const [developer, setDev] = useState({});
 
   const { id } = useParams();
+  console.log(id);
   
+
   useEffect(() => {
     let token = localStorage.getItem("token");
+    if (!token) {
+      console.log("Token topilmadi!");
+      return;
+    }
     axios
-      .get(`https://nt-devconnector.onrender.com/api/profile/${id}`, {
+      .get(`https://nt-devconnector.onrender.com/profile/${id}`, {
         headers: { "x-auth-token": token },
       })
-      .then((res) =>  setDev(res.data));
-  }, [id]);
+      .then((res) => setDev(res.data))
+      .catch((error) => console.log("Xatoilik yuz berdi", error.response?.data || error.mesage));
+  }, []);
 
   console.log(developer);
-  
 
   return (
     <div className="p-[20px]">
-        <div key={developer._id} className="border rounded p-[20px] mb-[10px]">
-          <h2>{developer.name}</h2>
-          <h2>{developer.location}</h2>
-        </div>
+      <div key={developer._id} className="border rounded p-[20px] mb-[10px]">
+        <h2>{developer?.name}</h2>
+        <h2>{developer?.location}</h2>
+      </div>
     </div>
   );
 };
